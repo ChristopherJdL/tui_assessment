b68698b82133c47475002cebb2985b4f86889ce2
Christophine <sharkchris91@gmail.com>
2018-03-30 17:48:03 +0200

    functional web app

--- part1/Controllers/FlightController.cs
+++ part1/Controllers/FlightController.cs
@@ -3,10 +3,11 @@ using System.Linq;
 using CodeInsider.Tui.Assessment.Data;
 using CodeInsider.Tui.Assessment.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodeInsider.Tui.Assessment.Controllers
 {
-    [Route("/[controller]")]
+    [Route("[controller]")]
     public class FlightController : Controller
     {
         public class FlightCreationInputModel
@@ -31,6 +32,12 @@ namespace CodeInsider.Tui.Assessment.Controllers
             this.FuelService = fuelService;
         }
 
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            var flight = this.DbContext.Flights.Where(f=> f.Id == id).Include(f => f.ArrivalAirport).Include(f => f.DepartureAirport);
+            return Ok(flight);
+        }
         [HttpPost]
         public IActionResult Post(FlightCreationInputModel input)
         {
@@ -48,15 +55,17 @@ namespace CodeInsider.Tui.Assessment.Controllers
         }
 
         [HttpPut]
-        public IActionResult Put(FlightEditInputModel input)
+        public IActionResult Put([FromForm] FlightEditInputModel input)
         {
             var flight = this.DbContext.Flights.Find(input.Id);
             var departureAirport = this.DbContext.Airports.Where(a => a.IataCode == input.DepartureIata).FirstOrDefault();
             var arrivalAirport = this.DbContext.Airports.Where(a => a.IataCode == input.ArrivalIata).FirstOrDefault();
-            if (flight == null)
-                return BadRequest("No flight found for given Id");
+            if (flight == null || departureAirport == null || arrivalAirport == null)
+                return BadRequest("No flight or airport found for given Id(s).");
             flight.ArrivalAirport = arrivalAirport;
             flight.DepartureAirport = departureAirport;
+            flight.FlightDistanceKilometers = this.DistanceResolver.ResolveDistance(departureAirport, arrivalAirport);
+            flight.FuelConsumptionLiters = this.FuelService.EstimateFuelConsumption(flight);
             DbContext.Update(flight);
             DbContext.SaveChanges();
             return Ok();
--- part1/wwwroot/js/flightOperations.js
+++ part1/wwwroot/js/flightOperations.js
@@ -1,4 +1,4 @@
-function addFlight(params) {
+function addFlight() {
     var airportCodeDeparture = $("#departure").find(":selected").val();
     var airportCodeArrival = $("#arrival").find(":selected").val();
     $.post("/Flight", {departureIata : airportCodeDeparture, arrivalIata: airportCodeArrival})
@@ -12,7 +12,27 @@ function addFlight(params) {
     
 }
 
-function editFlight()
+function getFlight(flightId)
 {
-    
+    var flight = null;
+    $.get("/Flight", {id: flightId}, function (data){
+        flight = data;
+    });
+    return flight;
+}
+
+function editFlight(flightId)
+{
+    var airportCodeDeparture = $("#departureModal").find(":selected").val();
+    var airportCodeArrival = $("#arrivalModal").find(":selected").val();
+    $.ajax({
+        method: "PUT",
+        accept: "application/json",
+        contentType: "application/x-www-form-urlencoded",
+        url: "/Flight",
+        data: { id: flightId, departureIata : airportCodeDeparture, arrivalIata: airportCodeArrival }
+      })
+      .done(function(){
+        alert("Successfully uploaded flight !")
+      });
 }
